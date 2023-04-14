f802cca2e3915de1085730a5ca752bb63c051966
Nicolas Pulti <nicopultigisondo@gmail.com>
2023-04-14 19:37:15 -0300

    Se agrego el metodo Buscar tarea por id y se cambio el tipo de retorno de los metodos a ResponseEntity

--- src/main/java/com/gestor/app/rest/Controller/TodoController.java
+++ src/main/java/com/gestor/app/rest/Controller/TodoController.java
@@ -2,13 +2,20 @@ package com.gestor.app.rest.Controller;
 
 import com.gestor.app.rest.Entity.Task;
 import com.gestor.app.rest.Repository.ITodoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TodoController {
+
+    private final Logger log = LoggerFactory.getLogger(TodoController.class);
     @Autowired
     private ITodoRepository todoRepository;
     // CRUD sobre la entidad Task
@@ -19,30 +26,68 @@ public class TodoController {
         return todoRepository.findAll();
     }
 
+    // Buscar tarea por id
+    @GetMapping(value = "/tasks/{id}")
+    public ResponseEntity<Task> findOneBydId(@PathVariable Long id){
+        Optional<Task> taskOpt = todoRepository.findById(id); // me permite recibir valores nulos
+        if(taskOpt.isPresent())
+        {
+            return ResponseEntity.ok(taskOpt.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
 
     // Crear un nuevo libro en base de datos
     @PostMapping(value = "/savetask")
-    public String saveTasl(@RequestBody Task task){
-        todoRepository.save(task);
-        return "Saved task";
+    public ResponseEntity<Task> saveTask(@RequestBody Task task, @RequestHeader HttpHeaders headers){
+        System.out.println(headers.get("User-Agent"));
+        if(task.getId()!=null){
+            log.warn("Trying to create task with id");
+            return ResponseEntity.badRequest().build();
+        }
+
+        Task result = todoRepository.save(task);
+        return ResponseEntity.ok(result);
     }
     // Actualizar libro existente en base de datos
     @PutMapping(value="/update/{id}")
-    public String updateTask(@PathVariable Long id,@RequestBody Task task){
-
-        Task updateTask = todoRepository.findById(id).get();
-        updateTask.setTitle(task.getTitle());
-        updateTask.setDescription(task.getDescription());
-        todoRepository.save(updateTask);
-        return "Updated task";
+    public ResponseEntity<Task> updateTask(@PathVariable Long id,@RequestBody Task task){
+        if(task.getId() == null){
+            log.warn("Triying to update a non existent task");
+            return ResponseEntity.badRequest().build();
+        }
+        if (!todoRepository.existsById(task.getId())){
+            log.warn("Triying to update a non existent task");
+            return ResponseEntity.notFound().build();
+        }
+        Task result = todoRepository.save(task);
+        return ResponseEntity.ok(result);
     }
 
     //Borrar libro por id en base de datos
-    @DeleteMapping(value="delete/{id}")
-    public String deleteTask(@PathVariable Long id){
-        Task deletedTask = todoRepository.findById(id).get();
-        todoRepository.delete(deletedTask);
-        return "Deletede Task";
+    @DeleteMapping(value="/delete/{id}")
+    public ResponseEntity<Task> deleteTask(@PathVariable Long id){
+        if(!todoRepository.existsById(id)){
+            log.warn("Triying to delete a non existent task");
+            return ResponseEntity.notFound().build();
+        }
+        todoRepository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    //Borrar todas las tareas
+    @DeleteMapping("/delete")
+    public ResponseEntity<Task> deleteAllTask(){
+
+        if(todoRepository.count()==0){
+            log.warn("The database is empty");
+        }
+        log.info("REST Request for delete all tasks");
+        todoRepository.deleteAll();
+
+        return ResponseEntity.noContent().build();
     }
 
 }
